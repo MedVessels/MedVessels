d5bf661cfc0361882fa737f8770f67a98c101beb
MedVessels <medvessels.xv.pt@gmail.com>
2023-07-01 20:43:31 +0100

    -add geojson poup

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ var portos = {
 	"name": "Portos",
 	"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 	"features": [
-	{ "type": "Feature", "properties": { "id": 1, "Nome": "Lisboa", "Tot Fretes": 13 }, "geometry": { "type": "Point", "coordinates": [ -9.242197253433176, 38.695380774032472 ] } },
-	{ "type": "Feature", "properties": { "id": 2, "Nome": "Tunísia", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 10.304249695235058, 36.805112857699399 ] } },
-	{ "type": "Feature", "properties": { "id": 3, "Nome": "Sicília", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 15.147467358686669, 36.86103924945327 ] } },
-	{ "type": "Feature", "properties": { "id": 4, "Nome": "Hyères", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 6.15914632498149, 43.08241667033348 ] } },
-	{ "type": "Feature", "properties": { "id": 5, "Nome": "Civitavecchia", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 11.771618460298964, 42.104331351410266 ] } },
-	{ "type": "Feature", "properties": { "id": 6, "Nome": "Córsega", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 8.760153517743181, 42.563418182374264 ] } },
-	{ "type": "Feature", "properties": { "id": 7, "Nome": "Génova", "Tot Fretes": 2 }, "geometry": { "type": "Point", "coordinates": [ 8.918817197291972, 44.398743391347203 ] } },
-	{ "type": "Feature", "properties": { "id": 8, "Nome": "La Spezia", "Tot Fretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 9.909678952254103, 44.071509380305571 ] } },
-	{ "type": "Feature", "properties": { "id": 9, "Nome": "Porto Pisano", "Tot Fretes": 7 }, "geometry": { "type": "Point", "coordinates": [ 10.2816615253092, 43.680844140362083 ] } },
-	{ "type": "Feature", "properties": { "id": 9, "Nome": "Pisa", "Tot Fretes": 7 }, "geometry": { "type": "Point", "coordinates": [ 10.268612409699587, 43.676866698822785 ] } }
+	{ "type": "Feature", "properties": { "id": 1, "Nome": "Lisboa", "TotFretes": 13 }, "geometry": { "type": "Point", "coordinates": [ -9.242197253433176, 38.695380774032472 ] } },
+	{ "type": "Feature", "properties": { "id": 2, "Nome": "Tunísia", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 10.304249695235058, 36.805112857699399 ] } },
+	{ "type": "Feature", "properties": { "id": 3, "Nome": "Sicília", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 15.147467358686669, 36.86103924945327 ] } },
+	{ "type": "Feature", "properties": { "id": 4, "Nome": "Hyères", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 6.15914632498149, 43.08241667033348 ] } },
+	{ "type": "Feature", "properties": { "id": 5, "Nome": "Civitavecchia", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 11.771618460298964, 42.104331351410266 ] } },
+	{ "type": "Feature", "properties": { "id": 6, "Nome": "Córsega", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 8.760153517743181, 42.563418182374264 ] } },
+	{ "type": "Feature", "properties": { "id": 7, "Nome": "Génova", "TotFretes": 2 }, "geometry": { "type": "Point", "coordinates": [ 8.918817197291972, 44.398743391347203 ] } },
+	{ "type": "Feature", "properties": { "id": 8, "Nome": "La Spezia", "TotFretes": 1 }, "geometry": { "type": "Point", "coordinates": [ 9.909678952254103, 44.071509380305571 ] } },
+	{ "type": "Feature", "properties": { "id": 9, "Nome": "Porto Pisano", "TotFretes": 7 }, "geometry": { "type": "Point", "coordinates": [ 10.2816615253092, 43.680844140362083 ] } },
+	{ "type": "Feature", "properties": { "id": 9, "Nome": "Pisa", "TotFretes": 7 }, "geometry": { "type": "Point", "coordinates": [ 10.268612409699587, 43.676866698822785 ] } }
 	]
 	}
 	
@@ -71,6 +71,9 @@ var Portos = L.geoJSON(portos, {
 			fillOpacity: 0.8
 			});
 			},
+		onEachFeature: function (feature,layer) {
+			layer.bindPopup(feature.properties.Nome+'</p>'+'Total de fretes: '+feature.properties.TotFretes)
+		}
 }).addTo(map);
 
 // Ajustar a exibição do mapa para se ajustar à camada de portos
